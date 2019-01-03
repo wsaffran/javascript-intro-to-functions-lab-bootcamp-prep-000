06b252a905bab029744b2c68685db27674ff76bb
William Saffran <willh.saffran@gmail.com>
2019-01-03 02:17:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,4 @@ function logWhisper(string) {
 sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
     return "I can't hear you!"
-  else if (string.toUpperCase === string)
-    return "YES INDEED!"
-  else if (string === "I love you, Grandma.")
-    return "I love you, too."
 }

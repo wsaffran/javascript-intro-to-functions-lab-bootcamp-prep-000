dae9a49d957d7d01aee5a71c6eb71ad90fc53cd0
William Saffran <willh.saffran@gmail.com>
2019-01-03 02:25:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string) {
+    return "I can't hear you!"
+  }
+  else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
